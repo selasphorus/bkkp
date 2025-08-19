8ae186b8c10175d84248ad85b1852c3f67c3b2d0
atc <birdhive@gmail.com>
2025-08-19 15:26:08 -0400

    ts

--- bkkp.php
+++ bkkp.php
@@ -49,6 +49,7 @@ add_action( 'whx4_pre_boot', function() {
 
         // Register the module with WHx4
         add_filter( 'whx4_register_modules', function( array $modules ): array {
+            error_log( '$$$ whx4_register_modules fired $$$' );
             return array_merge( $modules, [
                 'accounting' => Accounting::class,
                 //'employment' => Employment::class,
