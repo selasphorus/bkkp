8296f17ee2ef92fc2982f3b1e9bd5767a5c2f979
atc <birdhive@gmail.com>
2025-08-22 12:42:19 -0400

    oops removed to correct module (employment)

--- src/Modules/Accounting/AccountingModule.php
+++ src/Modules/Accounting/AccountingModule.php
@@ -6,7 +6,6 @@ use atc\WHx4\Core\Module as BaseModule;
 //
 use atc\Bkkp\Modules\Accounting\PostTypes\Account;
 use atc\Bkkp\Modules\Accounting\PostTypes\Transaction;
-use atc\Bkkp\Modules\Accounting\PostTypes\WorkPayment;
 //use atc\Bkkp\Modules\Accounting\PostTypes\LedgerEntry;
 
 // Define the module class
@@ -23,7 +22,6 @@ final class AccountingModule extends BaseModule
         return [
             Account::class,
             Transaction::class,
-            WorkPayment::class,
             //LedgerEntry::class,
         ];
     }
