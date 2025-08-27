4429bcbb4d34baa32524a98fc50d8f4fbe13405c
atc <birdhive@gmail.com>
2025-08-27 12:35:24 -0400

    cleanup

--- bkkp.php
+++ bkkp.php
@@ -28,14 +28,12 @@ if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 }
 
 use atc\WHx4\Plugin;
-//use atc\Bkkp\Plugin; // NO -- no separate plugin singleton needed -- use WHx4
-//use atc\Bkkp\Core\PostUtils;
+
 // TBD whether there's a way to streamline the following
+// Add-on Modules
 use atc\Bkkp\Modules\Accounting\AccountingModule as Accounting;
 use atc\Bkkp\Modules\Employment\EmploymentModule as Employment;
 //
-//use atc\Bkkp\Modules\Transactions\TransactionsModule as Transactions;
-//se atc\Bkkp\Modules\PayDocs\PayDocsModule as PayDocs;
 //use atc\Bkkp\Modules\TaxPrep\TaxPrepModule as TaxPrep;
 //use atc\Bkkp\Modules\Documents\DocumentsModule as Documents; // TODO: create separate mini-plugin to handle documents
 
@@ -80,56 +78,5 @@ add_action( 'whx4_pre_boot', function() {
        error_log( '$$$ Plugin class DNE $$$' );
     }
 }, 15 ); // Priority < 20 to run before WHx4 boot()
-
-// On activation, set up post types and capabilities
-/*
-// Once plugins are loaded, boot everything up
-add_action( 'plugins_loaded', function() {
-    Plugin::getInstance()->boot();
-}, 20 ); // Use a priority high enough to allow addons to hook before it runs
-
-// On activation, set up post types and capabilities
-register_activation_hook( __FILE__, function() {
-    $plugin = Plugin::getInstance();
-    $plugin->boot();
-});
-*/
-
-/*
-// Make sure we don't expose any info if called directly
-if ( !function_exists( 'add_action' ) ) {
-    echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
-    exit;
-}
-
-$plugin_path = plugin_dir_path( __FILE__ );
-*/
-
-/* +~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+ */
-/*
-// The old non-OOP way:
-// Get plugin options -- WIP
-$options = get_option( 'bkkp_settings' );
-if ( isset($options['bkkp_modules']) ) { $modules = $options['bkkp_modules']; } else { $modules = array( 'documents' ); }
-$includes = array( 'posttypes', 'taxonomies' );
-
-// Include sub-files
-// TODO: make them required? Otherwise dependencies may be an issue.
-// TODO: maybe: convert to classes/methods approach??
-foreach ( $includes as $inc ) {
-    $filepath = $plugin_path . 'inc/'.$inc.'.php';
-    if ( file_exists($filepath) ) { include_once( $filepath ); } else { echo "no $filepath found"; }
-}
-
-foreach ( $modules as $module ) {
-    $filepath = $plugin_path . 'modules/'.$module.'.php';
-    $arr_exclusions = array(); //$arr_exclusions = array ( 'admin_notes', 'data_tables', 'links', 'organizations', 'ensembles', 'organs', 'press', 'projects', 'sources' );
-    if ( !in_array( $module, $arr_exclusions) ) { // skip modules w/ no files
-        if ( file_exists($filepath) ) { include_once( $filepath ); } else { echo "module file $filepath not found"; }
-    }
-}
-*/
-/* +~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+ */
-
 
 ?>
