0fe1d2666c0abedfd94b01079c8fc863a10f0b5d
atc <birdhive@gmail.com>
2025-08-30 17:06:35 -0400

    NTS

--- src/Modules/TaxPrep/Taxonomies/ItemLabel.php
+++ src/Modules/TaxPrep/Taxonomies/ItemLabel.php
@@ -5,6 +5,7 @@ namespace atc\Bkkp\Modules\TaxPrep\Taxonomies;
 use atc\WHx4\Core\TaxonomyHandler;
 
 // For tax forms etc. e.g. "Federal income tax withheld", "Net Long-Term Capital Gain or (Loss)"
+// This is WIP -- to be integrated properly into repeater-row form building or similar
 class ItemLabel extends TaxonomyHandler
 {
     public function __construct(\WP_Term|null $term = null)
