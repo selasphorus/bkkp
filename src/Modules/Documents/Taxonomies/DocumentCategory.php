98e40ad08f583d3379c5618e706cf83f299597e1
atc <birdhive@gmail.com>
2025-08-30 10:36:32 -0400

    Added document_category taxonomy

---
+++ src/Modules/Documents/Taxonomies/DocumentCategory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace atc\Bkkp\Modules\Documents\Taxonomies;
+
+use atc\WHx4\Core\TaxonomyHandler;
+
+class DocumentCategory extends TaxonomyHandler
+{
+    public function __construct(\WP_Term|null $term = null)
+    {
+        parent::__construct([
+            'slug'         => 'document_category',
+            'plural_slug'  => 'document_categories',
+            'object_types' => ['document'],
+            'hierarchical' => true,
+        ], $term);
+    }
+}
