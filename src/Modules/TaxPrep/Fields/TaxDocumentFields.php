73153b80c94c8b93be3de0ccfb5f56fb3ff8f153
atc <birdhive@gmail.com>
2025-08-30 15:22:48 -0400

    Fixed copy/paste error

--- src/Modules/TaxPrep/Fields/TaxDocumentFields.php
+++ src/Modules/TaxPrep/Fields/TaxDocumentFields.php
@@ -9,20 +9,20 @@ use atc\WHx4\Core\SubtypeRegistrar;
 // TODO: rename all fields (keys/names) according to whx4 naming conventions
 final class TaxDocumentFields implements FieldGroupInterface, SubtypeFieldGroupInterface
 {
+    public function getPostType(): string
+    {
+        return 'document';
+    }
+
+    public function getSubtypeSlug(): string
+    {
+        return 'taxdocs';
+    }
+
     public static function register(): void
     {
         //error_log( '=== XXXFields: register()) ===' );
         if ( !function_exists('acf_add_local_field_group') ) return;
-
-        public function getPostType(): string
-        {
-            return 'document';
-        }
-
-        public function getSubtypeSlug(): string
-        {
-            return 'taxdocs';
-        }
 
         //use atc\WHx4\Migrations\FieldKeyMigrator;
         /*
