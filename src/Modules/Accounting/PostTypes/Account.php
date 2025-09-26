56937232d36cfeeb4e102f9aad882d43a6cf853a
atc <birdhive@gmail.com>
2025-09-26 15:03:39 -0400

    TS getRelatedPosts

--- src/Modules/Accounting/PostTypes/Account.php
+++ src/Modules/Accounting/PostTypes/Account.php
@@ -42,7 +42,7 @@ class Account extends PostTypeHandler
         $p = $post ?? $this->getPost();
         //return $p ? (string)get_post_meta($p->ID, 'account_status', true) : 'Unknown';
 
-        $related = getRelatedPosts( $post_id, 'transaction', 'account' ); // getRelatedPosts( $post_id = null, $related_post_type = null, $related_field_name = null, $limit = '1' )
+        $related = PostTypeHandler::getRelatedPosts( $post_id, 'transaction', 'account' ); // getRelatedPosts( $post_id = null, $related_post_type = null, $related_field_name = null, $limit = '1' )
 		/*if ( $arr_obj_transactions ) {
 
 			//$info .= "<h3>Transactions:</h3>";
