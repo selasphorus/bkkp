1d98e88ad22497139dbc05286acf3e1d7b9c741c
atc <birdhive@gmail.com>
2025-09-26 14:24:42 -0400

    new methods; view mods; WIP

--- src/Modules/Accounting/PostTypes/Account.php
+++ src/Modules/Accounting/PostTypes/Account.php
@@ -4,6 +4,7 @@ namespace atc\Bkkp\Modules\Accounting\PostTypes;
 
 use WP_Post;
 use atc\WHx4\Core\PostTypeHandler;
+use atc\Bkkp\Modules\Accounting\PostTypes\Transaction
 
 class Account extends PostTypeHandler
 {
@@ -23,10 +24,38 @@ class Account extends PostTypeHandler
 	    parent::boot(); // Optional if you add shared logic later
 	}
 
-    public function getStatus(?WP_Post $post = null): string
+    public function getStatus( ?WP_Post $post = null ): string
     {
         $p = $post ?? $this->getPost();
         return $p ? (string)get_post_meta($p->ID, 'account_status', true) : 'Unknown';
+    }
+
+    public function getStatements( ?WP_Post $post = null, $scope = "this_year" ): string
+    {
+        $p = $post ?? $this->getPost();
+        $related = getRelatedPosts( $post_id, 'transaction', 'account' ); // TODO: add 'scope' parameter
+        return $related;
+    }
+
+    public function getTransactions( ?WP_Post $post = null, $scope = "this_month"): string
+    {
+        $p = $post ?? $this->getPost();
+        //return $p ? (string)get_post_meta($p->ID, 'account_status', true) : 'Unknown';
+
+        $related = getRelatedPosts( $post_id, 'transaction', 'account' ); // getRelatedPosts( $post_id = null, $related_post_type = null, $related_field_name = null, $limit = '1' )
+		/*if ( $arr_obj_transactions ) {
+
+			//$info .= "<h3>Transactions:</h3>";
+
+			//$info .= "<p>arr_obj_transactions (".count($arr_obj_transactions)."): <pre>".print_r($arr_obj_transactions, true)."</pre></p>";
+			foreach ( $arr_obj_transactions as $transaction ) {
+				//$info .= $transaction->post_title."<br />";
+				// TODO: load table view...
+				$rep_info = Transaction::getSummary( $transaction->ID, 'display', false, true );
+				$info .= make_link( get_permalink($transaction->ID), $rep_info, "TEST rep title" )."<br />";
+			}
+		}*/
+		return $related;
     }
 }
 
--- src/Modules/Accounting/Views/Account/content.php
+++ src/Modules/Accounting/Views/Account/content.php
@@ -4,12 +4,12 @@ use atc\WHx4\Core\PostTypeHandler;
 /** @var WP_Post $post */
 $handler = PostTypeHandler::getHandlerForPost($post);
 
-// Example: Person-specific data
-$status = ($handler && method_exists($handler, 'getStatus'))
-    ? $handler->getStatus($post)
-    : '';
+// Account-specific data
+$status = ($handler && method_exists($handler, 'getStatus')) ? $handler->getStatus($post) : '';
+$transactions = ($handler && method_exists($handler, 'getTransactions')) ? $handler->getTransactions($post) : [];
 ?>
 
 <div>
 Account Status: <?php echo $status; ?>
+Transactions on record: <?php echo count($transactions); ?>
 </div>
