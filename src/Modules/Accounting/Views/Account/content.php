7e4850753ac25cd6ab67121c9cb26b548db1c1c8
atc <birdhive@gmail.com>
2025-09-26 17:55:55 -0400

    Additional info WIP

--- src/Modules/Accounting/Views/Account/content.php
+++ src/Modules/Accounting/Views/Account/content.php
@@ -3,6 +3,8 @@ use atc\WHx4\Core\PostTypeHandler;
 
 /** @var WP_Post $post */
 $handler = PostTypeHandler::getHandlerForPost($post);
+$pID = $handler->getPostID();
+$meta = $handler->getPostMeta();
 
 // Account-specific data
 $status = ($handler && method_exists($handler, 'getStatus')) ? $handler->getStatus($post) : '';
@@ -10,6 +12,8 @@ $transactions = ($handler && method_exists($handler, 'getTransactions')) ? $hand
 ?>
 
 <div>
-Account Status: <?php echo $status; ?>
+Account Status: <?php echo $status; ?><br />
 Transactions on record: <?php echo count($transactions); ?>
+<hr />
+Post Meta: <pre><?php print_r($meta,true); ?></pre>
 </div>
