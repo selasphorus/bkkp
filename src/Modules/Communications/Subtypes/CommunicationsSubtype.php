9944b16f2bcb26181370052f5bd1d7a854606b6a
atc-mba25 <birdhive@gmail.com>
2025-10-04 15:43:36 -0400

    cont.

--- src/Modules/Communications/Subtypes/CommunicationsSubtype.php
+++ src/Modules/Communications/Subtypes/CommunicationsSubtype.php
@@ -3,16 +3,19 @@
 namespace atc\Bkkp\Modules\Communications\Subtypes;
 
 use atc\WHx4\Core\Contracts\SubtypeInterface;
+use atc\WHx4\Core\Traits\SubtypeDefaults;
+use atc\WHx4\Core\Traits\SubtypeQueryHelpers;
 
 // TODO: rethink this implementation as a Subtype of Post
 // -- may want to build logbook module in case on some installations Post type is being used another way
 
 final class CommunicationsSubtype implements SubtypeInterface
 {
-    public function getPostType(): string
-    {
-        return 'post';
-    }
+    use SubtypeDefaults, SubtypeQueryHelpers;
+    
+    public const POST_TYPE = 'post';
+    public const TAXONOMY = 'category';
+    public const TERM = 'comms'; // ???
 
     public function getSlug(): string
     {
