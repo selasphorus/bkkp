038ea99ca89c64806689280c1b5152209b6dd701
atc-mba25 <birdhive@gmail.com>
2025-10-04 15:41:32 -0400

    cont.

--- src/Modules/TaxPrep/Subtypes/TaxDocumentsSubtype.php
+++ src/Modules/TaxPrep/Subtypes/TaxDocumentsSubtype.php
@@ -3,13 +3,16 @@
 namespace atc\Bkkp\Modules\TaxPrep\Subtypes;
 
 use atc\WHx4\Core\Contracts\SubtypeInterface;
+use atc\WHx4\Core\Traits\SubtypeDefaults;
+use atc\WHx4\Core\Traits\SubtypeQueryHelpers;
 
 final class TaxDocumentsSubtype implements SubtypeInterface
 {
-    public function getPostType(): string
-    {
-        return 'document';
-    }
+    use SubtypeDefaults, SubtypeQueryHelpers;
+    
+    public const POST_TYPE = 'document';
+    public const TAXONOMY = 'document_category';
+    public const TERM = 'taxes'; // ???
 
     public function getSlug(): string
     {
