3caa00796753a3adde1576172111a360c85d90d4
atc-mba25 <birdhive@gmail.com>
2025-10-05 08:15:37 -0400

    wip

--- src/Modules/Employment/Views/employment-income.php
+++ src/Modules/Employment/Views/employment-income.php
@@ -1,3 +1,9 @@
 <div class="whx4-employment">
 	<p><strong>Employers: ....</strong> <?php //echo (int)$stats['monsters']; ?></p>
+	<?php 
+	foreach ($posts as $post) {
+	    echo $post->post_title."<br />";
+	}
+	?>
+	<p><strong>Debug:</strong> <pre><?php echo print_r($debug, true); ?></pre></p>
 </div>
