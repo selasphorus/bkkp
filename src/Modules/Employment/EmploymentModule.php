dffc4e852c79fec2c10e2a1e9eb7db32d61b2c9e
atc-mba25 <birdhive@gmail.com>
2025-10-05 13:46:00 -0400

    cont.

--- src/Modules/Employment/EmploymentModule.php
+++ src/Modules/Employment/EmploymentModule.php
@@ -64,7 +64,7 @@ final class EmploymentModule extends BaseModule
 				['key' => 'group_category', 'value' => 'employers', 'compare' => '='],
 				//['key' => 'group_category', 'equals' => 'employers'], // TODO: enable this shorthand for tax queries
 			],
-			'limit'  => -1,
+			'limit'  => "-1",
 			//'per_page'  => -1,
 			'orderby'   => 'title',
 			'order'     => 'ASC',
