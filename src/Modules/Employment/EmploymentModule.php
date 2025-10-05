eeb81c75ef41132b5151fb6baab823e85aad573f
atc-mba25 <birdhive@gmail.com>
2025-10-05 18:03:31 -0400

    cont.

--- src/Modules/Employment/EmploymentModule.php
+++ src/Modules/Employment/EmploymentModule.php
@@ -67,7 +67,7 @@ final class EmploymentModule extends BaseModule
 			],
 			//'limit'  => "-1",
 			//'per_page'  => -1,
-			'posts_per_page' => 30,
+			'limit' => 30, //posts_per_page
 			'orderby'   => 'title',
 			'order'     => 'ASC',
 		], $options);
