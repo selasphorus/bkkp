d7a27ed9d12451f845f18ae0a2cd2a580f977e49
atc-mba25 <birdhive@gmail.com>
2025-10-06 07:56:07 -0400

    cont. ts

--- src/Modules/Accounting/Views/transactions-summary-grouped.php
+++ src/Modules/Accounting/Views/transactions-summary-grouped.php
@@ -18,7 +18,8 @@
 				<?php echo esc_html(get_the_title($employer)); ?>
 				</a-->
 				<?php
-				//echo "[".count($posts)."] transactions in the {$term} category, for a total of ${$sum}";
+				echo "[".count($posts)."] transactions";
+				echo " in the ".$term." category, for a total of $".$sum."";
 				/*foreach ( $docs as $doc ) {
 				    $h = $handler($doc);
 				    //$thing = $h->getPostMeta('start_date');
