45bda5c33e1203eb58616fdb573e95848cfc27ed
atc-mba25 <birdhive@gmail.com>
2025-10-06 11:00:30 -0400

    cont. ts

--- src/Modules/Accounting/Views/transactions-summary-grouped-catyears.php
+++ src/Modules/Accounting/Views/transactions-summary-grouped-catyears.php
@@ -4,7 +4,7 @@
 	<div class="troubleshooting">
         <h3>Info for Troubleshooting</h3>
         <p><?php echo $info; ?></p>
-        <!--p>atts: <pre><?php print_r($atts, true); ?></pre></p-->
+        <p>atts: <pre><?php print_r($atts, true); ?></pre></p>
         <!--debug: <pre><?php //print_r($debug, true); ?></pre>-->
     </div>
         <hr />
