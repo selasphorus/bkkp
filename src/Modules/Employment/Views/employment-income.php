cbe5831eb69f86d8512a882adff1fd54cf6ceeff
atc-mba25 <birdhive@gmail.com>
2025-10-06 07:46:53 -0400

    Disabled display of debugging info

--- src/Modules/Employment/Views/employment-income.php
+++ src/Modules/Employment/Views/employment-income.php
@@ -60,5 +60,5 @@
         <?php endif;*/ ?>
     <?php endif; ?>
     
-	<p><strong>Debug:</strong> <pre><?php echo print_r($debug, true); ?></pre></p>
+	<p><strong>Debug:</strong> <pre><?php //echo print_r($debug, true); ?></pre></p>
 </div>
