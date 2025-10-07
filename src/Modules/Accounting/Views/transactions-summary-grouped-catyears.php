c97daa01aede60112c52861574283fdffb1fc179
atc-mba25 <birdhive@gmail.com>
2025-10-07 18:06:52 -0400

    added missing echos

--- src/Modules/Accounting/Views/transactions-summary-grouped-catyears.php
+++ src/Modules/Accounting/Views/transactions-summary-grouped-catyears.php
@@ -4,8 +4,8 @@
 	<div class="troubleshootingg">
         <h3>Info for Troubleshooting</h3>
         <p>[info] <?php echo $info; ?></p>
-        <p>[atts] <pre><?php print_r($atts, true); ?></pre></p>
-        <p>[years] <pre><?php print_r($years, true); ?></pre></p>
+        <p>[atts] <pre><?php echo print_r($atts, true); ?></pre></p>
+        <p>[years] <pre><?php echo print_r($years, true); ?></pre></p>
     </div>
     <hr />
     
