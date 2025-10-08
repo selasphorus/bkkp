2e546926892df7a8c61be176708b87947ef70b3f
atc-mba25 <birdhive@gmail.com>
2025-10-08 17:17:55 -0400

    cont. styling

--- src/Modules/Accounting/Views/transactions-summary-grouped-catyears.php
+++ src/Modules/Accounting/Views/transactions-summary-grouped-catyears.php
@@ -27,7 +27,7 @@
 					<td><?php 
 					if ( $col['sum'] == "0.0" ) { echo "--"; } else { echo $col['sum']; }
 					// Show count if non-zero, even if sum is zero
-					if ( $col['count'] > 0 ) { echo '<span class="secondary">' . '(' . $col['count'] . ')' . '</span>'; }
+					if ( $col['count'] > 0 ) { echo '<span class="subtle">' . '(' . $col['count'] . ')' . '</span>'; }
 					?></td>
 				<?php endforeach; ?>
 				<?php 
