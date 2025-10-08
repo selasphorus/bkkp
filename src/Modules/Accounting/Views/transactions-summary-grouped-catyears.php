e7d108df66a62d5e7c11b52fd06f370ef115c75b
atc-mba25 <birdhive@gmail.com>
2025-10-08 16:03:29 -0400

    date query finally ok -- remove messy ts

--- src/Modules/Accounting/Views/transactions-summary-grouped-catyears.php
+++ src/Modules/Accounting/Views/transactions-summary-grouped-catyears.php
@@ -30,7 +30,7 @@
 				//echo "<pre>".print_r($row['cols'], true)."</pre>";
 				//echo "=> [".count($result['posts'])."] posts";
 				?>
-				<td><pre><?php echo print_r($row['result']['query_request'], true); ?></pre></td>
+				<td><pre><?php //echo print_r($row['result']['query_request'], true); ?></pre></td>
 			</tr>
 		<?php endforeach; ?>
 		</table>
