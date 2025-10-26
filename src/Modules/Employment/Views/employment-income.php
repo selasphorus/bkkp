acc44e4d1fcc447a84323b0de82a275f26968328
atc-mba25 <birdhive@gmail.com>
2025-10-25 22:00:29 -0400

    cont.

--- src/Modules/Employment/Views/employment-income.php
+++ src/Modules/Employment/Views/employment-income.php
@@ -120,7 +120,7 @@
 									<?php echo esc_html($comp_formatted); ?>
 								</a>
 								<?php if (!empty($total_withheld)): ?>
-									<span class="withheld-amount">
+									<span class="withheld-amount sceren-only">
 										($<?php echo esc_html(number_format((float)$total_withheld, 0)); ?> withheld)
 									</span>
 								<?php endif; ?>
