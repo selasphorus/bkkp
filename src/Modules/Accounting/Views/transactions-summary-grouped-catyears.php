2138586078548e9ba2280eb55130d250a31f4966
atc-mba25 <birdhive@gmail.com>
2025-10-27 13:26:04 -0400

    conspicuous styling for negative numbers

--- src/Modules/Accounting/Views/transactions-summary-grouped-catyears.php
+++ src/Modules/Accounting/Views/transactions-summary-grouped-catyears.php
@@ -62,12 +62,16 @@
 				<td><?php echo $row['term']->name; ?></td>
 				<?php foreach ($row['cols'] as $col): ?>
 					<td>
-					<?php // TODO: style according to whether sum is <> previous year ?>
+					<?php 
+					// TODO: style according to whether sum is <> previous year?
+					$txn_class = "numeric";
+					if ($col['sum'] < 0) { $txn_class .= "negative"; }
+					?>
 					<a href="<?php echo esc_url($col['url']); ?>" class="txn-total" target="_blank">
 					<?php if ($col['sum'] == 0.0): ?>
 						<span class="zero-amount">—</span>
 					<?php else: ?>
-						<span class="numeric">$<?php echo number_format($col['sum'], 2); ?></span>
+						<span class="<?php echo $txn_class; ?>">$<?php echo number_format($col['sum'], 2); ?></span>
 					<?php endif; ?>
 					<?php
 					// Show count if non-zero, even if sum is zero
