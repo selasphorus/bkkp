f3c48b66531a27d9c222bc09f666f79a140906a4
atc-mba25 <birdhive@gmail.com>
2025-10-27 14:39:57 -0400

    ts

--- src/Modules/Accounting/Views/transactions-summary-grouped-catyears.php
+++ src/Modules/Accounting/Views/transactions-summary-grouped-catyears.php
@@ -60,9 +60,9 @@
 		</tr>
 		<?php foreach ($rows as $row): ?>
 			<tr class="<?php echo $row['level'] > 0 ? 'child-category' : 'parent-category'; ?>">
-				<td class="category-name" style="<?php echo $row['level'] > 0 ? 'padding-left: 2em;' : ''; ?>">
+				<td class="category-name" style="<?php echo $row['level'] > 0 ? 'padding-left: ' . ($row['level'] * 2) . 'em;' : ''; ?>">
 					<?php if ($row['level'] > 0): ?>
-						<span class="indent-marker">↳ </span>
+						<span class="indent-marker"><?php echo str_repeat('↳ ', $row['level']); ?></span>
 					<?php endif; ?>
 					<?php echo esc_html($row['term']->name); ?>
 				</td>
