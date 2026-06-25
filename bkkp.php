90650aca5ef7236e92ec7342a11de8ad0b4abd6a
atc-mba25 <birdhive@gmail.com>
2026-06-25 16:44:14 -0400

    updated version num

--- bkkp.php
+++ bkkp.php
@@ -4,7 +4,7 @@
  * Description:       A WordPress plugin for personal bookkeeping
  * Dependencies:      Requires WHx4-Core, WHx4
  * Requires Plugins:  whx4-core, whx4
- * Version:           0.1.260409
+ * Version:           0.1.260625
  * Author:            atc
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
